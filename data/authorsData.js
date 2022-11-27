2b38d937620d6d4f850998b8c090d3287b8834d2
kitamreik <kitdamreik@gmail.com>
2022-11-26 18:16:48 -0700

    updating admin side console/done

--- data/authorsData.js
+++ data/authorsData.js
@@ -3,6 +3,21 @@ const { v4:uuid } = require('uuid');
 module.exports = [
   {
     _id: uuid(),
+    firstName:'Yesinia', 
+    lastName: 'JJ',
+    birthYear: 1998, 
+    bio: 'Yesinia is a graduate of University of Massachusetts Boston, does creative writing and has been learning how to engage and grow restoratively and transformatively with her presence in this cohort. She is a proud advocate for Black women, who face unfair power dynamics from men, challenging power in creative ways to shift the situation. Yesinia will be doing independent work, and showing and visiting class from time to time. Check a feature at: https://tj-practitioner-directory.cyclic.app/juneteenth-showcase'
+
+  },
+  {
+    _id: uuid(),
+    firstName:'Kit Fenrir', 
+    lastName: 'Amreik',
+    birthYear: 1994, 
+    bio: 'Kit Amreik (they/she) is a Full Stack Web Developer interested in learning and exploring back-end modalities, I am also a new-to-Denver transplant! I am an activist working to serve the community at large and creating gentle & tender realms of radical possibility modeling for all. Some of my core values are integrity, veracity and receptiveness. In order to practice activism, advocacy and real time praxis to support comrades, it is imperative to me to hold these tenets close to me as I do the work. In addition to the aforementioned, I am also a Global Restorative and Transformative Justice Practitioner and Facilitator, versed in facilitating harm circles and community accountability processes for folks, organizations and collectives; furthermore, I teach TJ, RJ and facilitation classes and I hold space for all to learn and engage with this concept, and then share and spark conversations within communities to transform themselves.'
+  },
+  {
+    _id: uuid(),
     firstName:'Solomon', 
     lastName: 'Northrup',
     birthYear: 1808, 
@@ -22,4 +37,5 @@ module.exports = [
     birthYear: 1916, 
     bio: 'Khaled Hosseini is an Afghan-American novelist, physician, activist, humanitarian, and UNHCR goodwill ambassador.'
   }
+  
 ];
