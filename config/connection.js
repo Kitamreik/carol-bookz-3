3d5e157fc748fd77cb079255f4a0823e58ec7ea2
kitamreik <kitdamreik@gmail.com>
2023-10-05 23:48:15 -0600

    mongoose upgrade in connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
-  if(!err) {
-    console.log("Successful connection with MongoDB Server");  
-  }
-  else {
-      console.log("Error with MongoDB's connectivity");
-      console.log(err)
-  }
-});
+mongoose.set('strictQuery', false);
+
+// mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
+//   if(!err) {
+//     console.log("Successful connection with MongoDB Server");  
+//   }
+//   else {
+//       console.log("Error with MongoDB's connectivity");
+//       console.log(err)
+//   }
+// });
+
+// NEW- VERSION 7
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.DB_URL)};
